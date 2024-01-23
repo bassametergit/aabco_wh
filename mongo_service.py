5af707a984eed6064554ba8697bdd9b745d4c8a1
bassam <bassam@anachrontech.com>
2024-01-23 21:51:50 +0200

    prod env added

--- mongo_service.py
+++ mongo_service.py
@@ -1,7 +1,11 @@
 from pymongo import MongoClient
+import os
 
 
 def get_collection(db_name: str, collection_name):
-    mongo_client = MongoClient('localhost', 27017)
+    if os.environ.get('ENV')=='local':
+        mongo_client = MongoClient('localhost', 27017)
+    else:
+        mongo_client = MongoClient(os.environ.get('DB_CONNECTION_STRING'))
     return mongo_client[db_name][collection_name]
 
