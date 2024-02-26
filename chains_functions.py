5d19f7fe462325ee785329931940cc14446868aa
bassam <bassam@anachrontech.com>
2024-02-26 10:19:48 +0200

    func11

--- chains_functions.py
+++ chains_functions.py
@@ -310,7 +310,7 @@ async def answer_one_session_question_streaming(query, pineconekey,openaik,index
     general_system_template = r""" 
       Please Use provided context to give a short answer to the question.
       If you cannot find the answer in the provided context, don't search anyehere else, just answer: I have no answer.
-      If the answer contains any relative time or date (like yesterday), try to search for information that allow you to provide absolute time or date (like on wednesday, February 26, 2024).
+      You can use other sources of information to do the following: if your answer contains any relative time or date (like yesterday), try to search provided context for information that allow you to infer and transform it into an absolute time or date.
       Always answer in the question's language. If the questions changes language, change also answer's language.
       ----
     {context}
